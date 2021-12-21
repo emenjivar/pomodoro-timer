7215e66d1b0a4cf4662e4bd9bb6c9eed5092b5f7
carlos menjivar <carlos.itca@gmail.com>
2021-12-21 16:25:58 -0600

    :lipstick: Add countDown widget

---
+++ app/src/main/java/com/emenjivar/pomodoro/CountDown.kt
@@ -0,0 +1,106 @@
+package com.emenjivar.pomodoro
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CountDown(
+    modifier: Modifier = Modifier,
+    time: String,
+    progress: Float,
+    size: Int,
+    stroke: Int
+) {
+    Column(modifier = modifier) {
+        Box {
+            CircularProgressIndicatorBackground(
+                modifier = Modifier
+                    .height(size.dp)
+                    .width(size.dp),
+                color = Color(0xFFFCD2D1),
+                stroke = stroke
+            )
+
+            CircularProgressIndicator(
+                progress = progress,
+                modifier = Modifier
+                    .height(size.dp)
+                    .width(size.dp),
+                color = Color(0xFFFF5C58),
+                strokeWidth = stroke.dp
+            )
+
+            Column(modifier = Modifier.align(Alignment.Center)) {
+                Text(
+                    text = time,
+                    color = Color(0xFFFF5C58),
+                    style = MaterialTheme.typography.h2,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CircularProgressIndicatorBackground(
+    modifier: Modifier = Modifier,
+    color: Color,
+    stroke: Int
+) {
+    val style = with(LocalDensity.current) {
+        Stroke(stroke.dp.toPx())
+    }
+
+    Canvas(
+        modifier = modifier
+    ) {
+        val innerRadius = (size.minDimension - style.width) / 2
+
+        drawArc(
+            color = color,
+            startAngle = 0f,
+            sweepAngle = 360f,
+            useCenter = false,
+            size = Size(innerRadius * 2, innerRadius * 2),
+            topLeft = Offset(
+                (size / 2.0f).width - innerRadius,
+                (size / 2.0f).height - innerRadius
+            ),
+            style = style
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMyCanvas() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CountDown(
+            modifier = Modifier
+                .padding(top = 50.dp),
+            time = "24:59",
+            progress = 0.98f,
+            size = 300,
+            stroke = 8
+        )
+    }
+}
