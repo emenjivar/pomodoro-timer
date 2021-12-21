6ae73d9e9c2819d1879c69343209ecd7ecd34934
carlos menjivar <carlos.itca@gmail.com>
2021-12-21 16:36:04 -0600

    :lipstick: Add colors on resources

--- app/src/main/java/com/emenjivar/pomodoro/CountDown.kt
+++ app/src/main/java/com/emenjivar/pomodoro/CountDown.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +32,7 @@ fun CountDown(
                 modifier = Modifier
                     .height(size.dp)
                     .width(size.dp),
-                color = Color(0xFFFCD2D1),
+                color = colorResource(id = R.color.light),
                 stroke = stroke
             )
 
@@ -40,7 +41,7 @@ fun CountDown(
                 modifier = Modifier
                     .height(size.dp)
                     .width(size.dp),
-                color = Color(0xFFFF5C58),
+                color = colorResource(id = R.color.primary),
                 strokeWidth = stroke.dp
             )
 
