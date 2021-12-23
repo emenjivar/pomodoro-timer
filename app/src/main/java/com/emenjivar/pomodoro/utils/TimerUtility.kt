b6968d0df4812b171553109245117a09650391ec
carlos menjivar <carlos.itca@gmail.com>
2021-12-22 21:19:46 -0600

    :sparkles: Calculate pomodoro time progress

--- app/src/main/java/com/emenjivar/pomodoro/utils/TimerUtility.kt
+++ app/src/main/java/com/emenjivar/pomodoro/utils/TimerUtility.kt
@@ -4,14 +4,22 @@ import java.util.concurrent.TimeUnit
 
 object TimerUtility {
 
-    // Value on milliseconds
+    // Default value of pomodoro is 25 minutes
     const val POMODORO_TIME: Long = 1000 * 60 * 25
     private const val TIME_FORMAT = "%02d:%02d"
 
-    // Convert milliseconds to readable time
+    /**
+     * Convert milliseconds to readable time
+     * 1500000ms -> 25:00
+     */
     fun Long.formatTime() = String.format(
         TIME_FORMAT,
         TimeUnit.MILLISECONDS.toMinutes(this),
         TimeUnit.MILLISECONDS.toSeconds(this) % 60
     )
+
+    /**
+     * Calculate percent in pomodoro elapsed time
+     */
+    fun getProgress(currentTime: Long, totalTime: Long = POMODORO_TIME): Long = currentTime * 100 / totalTime
 }
--- app/src/test/java/com/emenjivar/pomodoro/utils/TimerUtilityTest.kt
+++ app/src/test/java/com/emenjivar/pomodoro/utils/TimerUtilityTest.kt
@@ -9,13 +9,19 @@ class TimerUtilityTest{
     @Test
     fun `formatTime parse complete pomodoro time`() {
         val time = TimerUtility.POMODORO_TIME
+        val progress = TimerUtility.getProgress(time)
+
         assertEquals("25:00", time.formatTime())
+        assertEquals(100L, progress)
     }
 
     @Test
     fun `formatTime parse pomodoro time less one second`() {
         val time = TimerUtility.POMODORO_TIME - 1000
+        val progress = TimerUtility.getProgress(time)
+
         assertEquals("24:59", time.formatTime())
+        assertEquals(99L, progress)
     }
 
     @Test
@@ -33,12 +39,33 @@ class TimerUtilityTest{
     @Test
     fun `formatTime parse half pomodoro time`() {
         val time  = TimerUtility.POMODORO_TIME / 2
+        val progress = TimerUtility.getProgress(time)
+
         assertEquals(time.formatTime(), "12:30")
+        assertEquals(50L, progress)
     }
 
     @Test
     fun `formatTime parse double pomodoro time`() {
         val time = TimerUtility.POMODORO_TIME * 2
+        val progress = TimerUtility.getProgress(time, time)
+
         assertEquals(time.formatTime(), "50:00")
+        assertEquals(100L, progress)
+    }
+
+    @Test
+    fun `getProgress calculate several percents`() {
+        val time100Percent = TimerUtility.getProgress(TimerUtility.POMODORO_TIME)
+        val time75Percent = TimerUtility.getProgress((TimerUtility.POMODORO_TIME * 0.75).toLong())
+        val time50Percent = TimerUtility.getProgress(TimerUtility.POMODORO_TIME / 2)
+        val time25Percent = TimerUtility.getProgress((TimerUtility.POMODORO_TIME * 0.25).toLong())
+        val time0Percent = TimerUtility.getProgress(0L)
+
+        assertEquals(100L, time100Percent)
+        assertEquals(75L, time75Percent)
+        assertEquals(50L, time50Percent)
+        assertEquals(25L, time25Percent)
+        assertEquals(0L, time0Percent)
     }
 }
