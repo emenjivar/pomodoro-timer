fe8ef95b68146ec55cfe63e8ea6899ea0601a714
carlos menjivar <carlos.itca@gmail.com>
2021-12-29 16:00:12 -0600

    :truck: Rename model

--- app/src/main/java/com/emenjivar/pomodoro/model/Counter.kt
+++ app/src/main/java/com/emenjivar/pomodoro/model/Counter.kt
@@ -3,7 +3,7 @@ package com.emenjivar.pomodoro.model
 import com.emenjivar.pomodoro.utils.TimerUtility
 import com.emenjivar.pomodoro.utils.TimerUtility.formatTime
 
-data class Counter(
+data class Pomodoro(
     val milliseconds: Long = TimerUtility.POMODORO_TIME,
     val time: String = TimerUtility.POMODORO_TIME.formatTime(),
     val progress: Float = 100f
