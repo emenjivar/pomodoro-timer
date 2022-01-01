512d91a5315fe1f1eaef1e50f70e7915e8222444
carlos menjivar <carlos.itca@gmail.com>
2022-01-01 10:44:09 -0600

    :hammer: Add color to script output

--- before_push.sh
+++ before_push.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Linter
+echo -e "\n\e[93mExecuting linter..."
 ./gradlew lint
 
 # Kotlin linter
+echo -e "\n\e[93mExecuting kotlin linter..."
 ./gradlew ktlintCheck
 
 # Unit testing
+echo -e "\n\e[93mExecuting unit tests..."
 ./gradlew testDebugUnitTest
