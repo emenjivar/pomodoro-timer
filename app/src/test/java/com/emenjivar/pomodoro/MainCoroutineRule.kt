f17fe3d5dd077d5649606168bb87bcf33c6b3935
carlos menjivar <carlos.itca@gmail.com>
2022-01-29 21:53:56 -0600

    :sparkles: Update tests

---
+++ app/src/test/java/com/emenjivar/pomodoro/MainCoroutineRule.kt
@@ -0,0 +1,24 @@
+package com.emenjivar.pomodoro
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineExceptionHandler
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.createTestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class MainCoroutineRule(private val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()) :
+    TestWatcher(), TestCoroutineScope by createTestCoroutineScope(TestCoroutineDispatcher() + TestCoroutineExceptionHandler() + dispatcher) {
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
--- app/src/test/java/com/emenjivar/pomodoro/screens/settings/SettingsViewModelTest.kt
+++ app/src/test/java/com/emenjivar/pomodoro/screens/settings/SettingsViewModelTest.kt
@@ -1,23 +1,94 @@
 package com.emenjivar.pomodoro.screens.settings
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.emenjivar.core.repository.SettingsRepository
+import com.emenjivar.core.usecase.GetPomodoroTimeUseCase
+import com.emenjivar.core.usecase.GetRestTimeUseCase
+import com.emenjivar.core.usecase.SetPomodoroTimeUseCase
+import com.emenjivar.core.usecase.SetRestTimeUseCase
+import com.emenjivar.pomodoro.MainCoroutineRule
 import com.emenjivar.pomodoro.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
+import org.mockito.Mockito
 
 class SettingsViewModelTest {
 
-    private val settingsViewModel = SettingsViewModel()
+    private lateinit var getPomodoroTimeUseCase: GetPomodoroTimeUseCase
+    private lateinit var setPomodoroTimeUseCase: SetPomodoroTimeUseCase
+    private lateinit var getRestTimeUseCase: GetRestTimeUseCase
+    private lateinit var setRestTimeUseCase: SetRestTimeUseCase
+    private lateinit var settingsRepository: SettingsRepository
+
+    private lateinit var settingsViewModel: SettingsViewModel
 
     @get:Rule
     val rule: TestRule = InstantTaskExecutorRule()
 
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    @ExperimentalCoroutinesApi
+    @Before
+    fun prepareTest() = mainCoroutineRule.runBlockingTest {
+
+        settingsRepository = Mockito.mock(SettingsRepository::class.java)
+        getPomodoroTimeUseCase = GetPomodoroTimeUseCase(settingsRepository)
+        setPomodoroTimeUseCase = SetPomodoroTimeUseCase(settingsRepository)
+        getRestTimeUseCase = GetRestTimeUseCase(settingsRepository)
+        setRestTimeUseCase = SetRestTimeUseCase(settingsRepository)
+
+        Mockito.`when`(getPomodoroTimeUseCase.invoke()).thenReturn(25000)
+        Mockito.`when`(getRestTimeUseCase.invoke()).thenReturn(5000)
+
+        settingsViewModel = SettingsViewModel(
+            getPomodoroTimeUseCase,
+            setPomodoroTimeUseCase,
+            getRestTimeUseCase,
+            setRestTimeUseCase,
+            Dispatchers.Main
+        )
+    }
+
     @Test
     fun `test default values`() {
         assertFalse(settingsViewModel.closeSettings.value ?: true)
+
+        // Values loaded on init
+        assertEquals(25000L, settingsViewModel.pomodoroTime.getOrAwaitValue())
+        assertEquals(5000L, settingsViewModel.restTime.getOrAwaitValue())
+    }
+
+    @Test
+    fun `setPomodoroTime using string parameter`() {
+        settingsViewModel.setPomodoroTime("10")
+        assertEquals(10L, settingsViewModel.pomodoroTime.getOrAwaitValue())
+    }
+
+    @Test
+    fun `setPomodoroTime using an invalid string parameter`() {
+        settingsViewModel.setPomodoroTime("NaN")
+        assertEquals(0L, settingsViewModel.pomodoroTime.getOrAwaitValue())
+    }
+
+    @Test
+    fun `setRestTime using string parameter`() {
+        settingsViewModel.setRestTime("10")
+        assertEquals(10L, settingsViewModel.restTime.getOrAwaitValue())
+    }
+
+    @Test
+    fun `setRestTime using an invalid string parameter`() {
+        settingsViewModel.setRestTime("NaN")
+        assertEquals(0L, settingsViewModel.restTime.getOrAwaitValue())
     }
 
     @Test
@@ -28,3 +99,4 @@ class SettingsViewModelTest {
         Assert.assertTrue(result)
     }
 }
+
