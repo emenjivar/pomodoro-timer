ceba87fd1e4f0ff8a966eaa52dcc687bfd8b23f4
carlos menjivar <carlos.itca@gmail.com>
2022-04-16 23:23:07 -0600

    :lipstick: Update switch color

--- app/src/main/java/com/emenjivar/pomodoro/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/emenjivar/pomodoro/screens/settings/SettingsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
@@ -23,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
@@ -185,7 +187,11 @@ fun SwitchItem(
         ) {
             Switch(
                 checked = autoPlay,
-                onCheckedChange = onCheckedChange
+                onCheckedChange = onCheckedChange,
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = colorResource(R.color.primary),
+                    uncheckedThumbColor = colorResource(R.color.light_gray)
+                )
             )
         }
     }
