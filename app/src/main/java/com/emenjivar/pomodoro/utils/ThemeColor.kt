336bfac27b94d04fe88edee6df2220076c98ee0b
carlos menjivar <carlos.itca@gmail.com>
2022-05-01 19:31:53 -0600

    :necktie: Create class to handle theme colors

--- app/src/main/java/com/emenjivar/pomodoro/utils/ThemeColor.kt
+++ app/src/main/java/com/emenjivar/pomodoro/utils/ThemeColor.kt
@@ -4,9 +4,9 @@ import androidx.annotation.ColorRes
 import com.emenjivar.pomodoro.R
 
 sealed class ThemeColor(@ColorRes val color: Int) {
-    class Tomato: ThemeColor(R.color.primary)
-    class Orange: ThemeColor(R.color.orange)
-    class Wine: ThemeColor(R.color.wine)
-    class Basil: ThemeColor(R.color.basil)
-    class Charcoal: ThemeColor(R.color.charcoal)
+    class Tomato : ThemeColor(R.color.primary)
+    class Orange : ThemeColor(R.color.orange)
+    class Wine : ThemeColor(R.color.wine)
+    class Basil : ThemeColor(R.color.basil)
+    class Charcoal : ThemeColor(R.color.charcoal)
 }
