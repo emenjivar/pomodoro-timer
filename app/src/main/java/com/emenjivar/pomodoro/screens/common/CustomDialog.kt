c140202da27629ccade43edb5cd1e4fc9fb781fb
Carlos Menjivar <carlos.itca@gmail.com>
2022-06-13 21:46:38 -0600

    :lipstick: Set textColor modal on darkMode

--- app/src/main/java/com/emenjivar/pomodoro/screens/common/CustomDialog.kt
+++ app/src/main/java/com/emenjivar/pomodoro/screens/common/CustomDialog.kt
@@ -1,5 +1,6 @@
 package com.emenjivar.pomodoro.screens.common
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -23,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -68,6 +70,7 @@ fun CustomDialog(
                         keyboardType = KeyboardType.Number
                     ),
                     colors = TextFieldDefaults.textFieldColors(
+                        textColor = R.color.black.toColor(),
                         backgroundColor = R.color.white.toColor(),
                         focusedIndicatorColor = R.color.primary.toColor()
                     )
@@ -109,4 +112,16 @@ private fun CustomTextButton(
     ) {
         Text(text)
     }
+}
+
+@Preview(name = "Night mode", uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun CustomDialogPreview() {
+    CustomDialog(
+        defaultValue = "25:00",
+        title = "Work time",
+        subtitle = "Please enter the time in minutes",
+        onSaveItem = {},
+        onDismiss = {}
+    )
 }
